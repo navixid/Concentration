0a0058405a2491a4425cf0f905e88fe6bdf715a6
navix <naviap@yandex.ru>
2024-05-28 13:01:44 +0300

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by navi on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
