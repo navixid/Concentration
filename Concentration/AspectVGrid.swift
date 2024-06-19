ebdf018523c2ae2626504475f7c14db0549cdbd3
navixid <naviap@yandex.ru>
2024-06-19 17:50:18 +0300

    new ViewBuilder

---
+++ Concentration/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Concentration
+//
+//  Created by navi on 19.06.2024.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
